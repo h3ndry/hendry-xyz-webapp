74d9b1bf85cae8b39128ecd5e6a7386738e7786b
Hendry <h3ndry@users.noreply.github.com>
2021-12-05 12:59:02 +0000

    lets try now

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
-import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	extensions: ['.svelte'],
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: netlify(),
-		target: '#svelte'
+		target: '#svelte',
+		ssr: true,
+		vite: {}
 	}
 };
 
