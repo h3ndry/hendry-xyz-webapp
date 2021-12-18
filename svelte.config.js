d2afb719a83e95a4cf704be5f09cbde2c0126329
Hendry <h3ndry@users.noreply.github.com>
2021-12-18 16:24:08 +0000

    rm adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
+	
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: netlify(),
-		target: '#svelte'
+		// adapter: adapter(),
+		target: '#svelte',
 	}
 };
 
