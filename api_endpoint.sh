40fdb1bac514c629c7c3418558be96c6148e1f01
Hendry <h3ndry@users.noreply.github.com>
2022-02-18 08:24:43 +0000

    added "/about" endpoint

--- api_endpoint.sh
+++ api_endpoint.sh
@@ -5,6 +5,6 @@ curl "$base_url/posts" | jq
 
 curl -g "$base_url/posts?filters[slug][$eq]=post-1" | jq
 
-curl "$base_url/about-page" | jq
+curl "$base_url/about-page"
 
 curl "$base_url/projects?populate=*" | jq
